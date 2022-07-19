f8e1d920e093fb0937eda047f632ab3ae53b7260
balbboa <abmm_arthur@hotmail.com>
2022-07-19 11:24:53 -0300

    Feat: rebranding

--- pages/escalas/add-escala.tsx
+++ pages/escalas/add-escala.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/jsx-key */
 import { Button } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import CardPosto from "../../components/CardAddPosto";
 import CardInfo from "../../components/CardEscala";
 import { Row } from "../../components/CardEscala/Card.styles";
@@ -8,21 +9,14 @@ import { Tittle } from "../../components/Container/Container.Styles";
 import withAuth from "../../utils/withAuth";
 
 function AddEscalas() {
-  const [postos, setPostos] = useState<any>([]);
   const [cont, setCont] = useState<any>(0);
 
   function AddPosto() {
     setCont(cont + 1)
-    setPostos(postos?.concat(<li id={cont} key={cont}><CardPosto /></li>));
   }
 
-  useEffect(() => {
-    const cardsPosto: any | null = document.getElementById("postos")?.getElementsByClassName("card")
-    for (let i = 0; i < cardsPosto.length; i++) {
-      cardsPosto[i].id = `posto${i}`;
-    }
-  })
-
+  const listItems = [...Array(cont)].map((_, i) => <CardPosto key={i} />)
+  
   return (
     <Container title="Adicionar Escala">
       <Tittle>Adicionar Escala</Tittle>
@@ -35,9 +29,7 @@ function AddEscalas() {
           Adicionar Posto
         </Button>
       </Row>
-      <ul id="postos">
-        {postos}
-      </ul>
+      {listItems}
     </Container>
   );
 }
